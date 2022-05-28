081debfbfb7e33786f8ac5f93e1ce1b2c1fb34ee
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-05-28 10:09:30 -0700

    Add files via upload

---
+++ calulator.py
@@ -0,0 +1,9 @@
+#This session is used for a simple calculator
+print('Here are some option that can be used according to the user interest \n (1)addition \n (2) subtration \n (3)division \n (4)multipliation ')
+a = int(input("Enter the number?"))
+if a==1:
+    a=input()
+elif a==2:
+    print("number is equal to 50");
+elif a==3:
+    print("number is equal to 100");
