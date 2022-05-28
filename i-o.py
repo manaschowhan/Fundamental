160694e9a200596fcced2d644aa793be7747b08b
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-05-28 10:01:33 -0700

    Add files via upload

---
+++ i-o.py
@@ -0,0 +1,7 @@
+#This session will be used for receiving the of input values of the user and then executing them during the run time
+a=input('Enter the values that you want to be displayed on the screen (it may be integer of of any other form) ')
+print('The value that has been entered is ',a)
+print('The Simple addition can be one this way')
+a,b=input('Enter two integers to be added ')
+c= int(a)+int(b)
+print('The sum of',a, 'and' ,b, 'is',c)
