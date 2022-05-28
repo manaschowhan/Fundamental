4ae76f3d5de245784c548f21cff94604ef868778
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-05-28 09:56:48 -0700

    Add files via upload

---
+++ BodmasOperation.py
@@ -0,0 +1,21 @@
+#Operation of Addition
+print('The addition of two integers of 5 and 6 are')
+print(5+6)
+#Operation of Subtration
+print('The Subtration of two integer suh as 5 and 4 is')
+print(5-4)
+#Operation of Multipliation
+print('The Multipliation of two integers such as 6 and 6 is')
+print(6*6)
+#Operation of Division
+print('The Division of Two integers such as 10 and 3 is')
+print(10/3)
+#Oeration using Algebra
+print('The answer of the following numerial expression that is 2*(10+5) will be outputed as')
+print(2*(10+5))
+
+#In the other way it ll bw outputed as
+print('The answer of the following numerial expression that is (2*10)+5 will be outputed as')
+print((2*10)+5)
+print('Thats all forthe bodmas part')
+
