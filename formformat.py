7b6831fef9378ab95d887f8a0d56cc2273ab9582
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-05-28 10:08:17 -0700

    Delete formformat.py

--- formformat.py
+++
@@ -1,6 +0,0 @@
-#This session can be used as a form.....
-print('*Enter the values as asked below*')
-a=input('Enter your Name: ')
-b=input('Enter Your age: ')
-c=input('Enter your Gender (F/M): ')
-print('Your name is ',a,'\n Your Age is ',b,'\n Your gender is ',c)
