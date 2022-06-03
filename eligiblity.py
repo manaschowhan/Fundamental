504cc08469588f8be3ace464f345454dfda86cf4
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-06-03 11:36:15 -0700

    Add files via upload

---
+++ eligiblity.py
@@ -0,0 +1,7 @@
+#In this session we will check the condition and give the output
+a=input('Enter your name.... ')
+b=int(input('Enter your age... '))
+if b<=17:
+    print(a,',Your not Eligible to vote')
+else:
+    print(a,',You are Eligible to vote')
