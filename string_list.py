2fa549c4bb7ffaa076f128aeda41048d0a4942dc
manaschowhan <106435245+manaschowhan@users.noreply.github.com>
2022-06-03 11:25:02 -0700

    Add files via upload

---
+++ string_list.py
@@ -0,0 +1,8 @@
+#In this session we will be introduced to create a list of string from the input string...
+i = 0
+str1 = input('Enter the string to be listed... ')
+print('The letters will be listed as follows.... ')
+
+while i < len(str1):
+    print( str1[i])
+    i += 1
